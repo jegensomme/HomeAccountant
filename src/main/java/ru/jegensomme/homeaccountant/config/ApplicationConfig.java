c3bc0c069e0f32b2ccf280b4a9469f371cd9b676
vitov <vitovt.kublitsky@mail.ru>
2021-10-19 12:45:45 +0300

    Added ApplicationConfig.

---
+++ src/main/java/ru/jegensomme/homeaccountant/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package ru.jegensomme.homeaccountant.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.jegensomme.homeaccountant")
+public class ApplicationConfig {
+}
