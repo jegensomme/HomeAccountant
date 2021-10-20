7c9f67d4f9625010ccf1c35a15c87ca28781df49
vitov <vitovt.kublitsky@mail.ru>
2021-10-20 15:57:58 +0300

    Added monthly limit of expenses.

--- src/main/java/ru/jegensomme/homeaccountant/model/User.java
+++ src/main/java/ru/jegensomme/homeaccountant/model/User.java
@@ -3,9 +3,11 @@ package ru.jegensomme.homeaccountant.model;
 import lombok.*;
 import org.hibernate.annotations.BatchSize;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -36,6 +38,10 @@ public class User extends NamedEntity {
     @javax.validation.constraints.NotNull
     private @NotNull Date registered = new Date();
 
+    @Column(name = "monthly_limit")
+    @Min(0)
+    private @Nullable Integer monthlyLimit;
+
     @Enumerated(EnumType.STRING)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
             uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role"}, name = "user_roles_unique_idx")})
@@ -45,22 +51,26 @@ public class User extends NamedEntity {
     private @NotNull Set<Role> roles;
 
     public User(@NotNull Integer id,
-                @NotNull String name,
-                @NotNull String email,
-                @NotNull String password,
-                @NotNull Role role,
-                @NotNull Role... roles) {
-        this(id, name, email, password, EnumSet.of(role, roles));
+                @NotNull String name, @NotNull String email, @NotNull String password,
+                @NotNull Role role, @NotNull Role... roles) {
+        this(id, name, email, password, null, EnumSet.of(role, roles));
     }
 
     public User(@NotNull Integer id,
-                @NotNull String name,
-                @NotNull String email,
-                @NotNull String password,
+                @NotNull String name, @NotNull String email, @NotNull String password,
+                @Nullable Integer monthlyLimit,
+                @NotNull Role role, @NotNull Role... roles) {
+        this(id, name, email, password, monthlyLimit, EnumSet.of(role, roles));
+    }
+
+    public User(@NotNull Integer id,
+                @NotNull String name, @NotNull String email, @NotNull String password,
+                @Nullable Integer monthlyLimit,
                 @NotNull Set<Role> roles) {
         super(id, name);
         this.email = email;
         this.password = password;
+        this.monthlyLimit = monthlyLimit;
         this.roles = roles;
     }
 }
--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -8,11 +8,12 @@ CREATE SEQUENCE global_seq START WITH 100000;
 
 CREATE TABLE users
 (
-    id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    name       VARCHAR                           NOT NULL,
-    email      VARCHAR                           NOT NULL,
-    password   VARCHAR                           NOT NULL,
-    registered TIMESTAMP           DEFAULT now() NOT NULL
+    id           INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name         VARCHAR                           NOT NULL,
+    email        VARCHAR                           NOT NULL,
+    password     VARCHAR                           NOT NULL,
+    registered   TIMESTAMP           DEFAULT now() NOT NULL,
+    monthly_limit INTEGER
 );
 
 CREATE TABLE user_roles
