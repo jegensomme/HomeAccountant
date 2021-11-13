8a0931c75513b12657b377b733c8f40214b50dd0
vitov <vitovt.kublitsky@mail.ru>
2021-11-13 18:18:45 +0300

    Fix, split SecurityConfig into rest and app

--- src/main/java/ru/jegensomme/homeaccountant/config/SecurityConfig.java
+++ src/main/java/ru/jegensomme/homeaccountant/config/SecurityConfig.java
@@ -1,23 +1,27 @@
 package ru.jegensomme.homeaccountant.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import ru.jegensomme.homeaccountant.service.UserService;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+@Import({SecurityConfig.RestSecurityConfig.class, SecurityConfig.ApplicationSecurityConfig.class})
+public class SecurityConfig {
     private final UserService userService;
 
     @Bean
@@ -25,43 +29,57 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/resources/**", "/webjars/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new CharacterEncodingFilter("UTF-8", true), CsrfFilter.class);
-        http.csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/rest/admin/**").hasRole("ADMIN")
-                    .antMatchers("/rest/**").authenticated()
-                .and()
-                    .httpBasic()
-                .and().authorizeRequests()
-                    .antMatchers("/admin/**").hasRole("ADMIN")
-                    .antMatchers("/**").authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .failureUrl("/login?error=true")
-                    .defaultSuccessUrl("/expenses")
-                    .loginProcessingUrl("/spring_security_check")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .logoutSuccessUrl("/login")
-                    .permitAll();
-    }
-
-
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userService);
         authProvider.setPasswordEncoder(passwordEncoder());
         auth.authenticationProvider(authProvider);
+    }
+
+    @Configuration
+    @Order(1)
+    public static class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/rest/**")
+                    .csrf().disable()
+                    .httpBasic(Customizer.withDefaults())
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/rest/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+            ;
+        }
+    }
+
+    @Configuration
+    public static class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(WebSecurity web) {
+            web.ignoring().antMatchers("/resources/**", "/webjars/**");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .authorizeRequests()
+                        .antMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                    .and()
+                        .formLogin()
+                        .loginPage("/login")
+                        .failureUrl("/login?error=true")
+                        .defaultSuccessUrl("/expenses")
+                        .loginProcessingUrl("/spring_security_check")
+                        .permitAll()
+                    .and()
+                        .logout()
+                        .logoutSuccessUrl("/login")
+                    .permitAll();
+        }
     }
 }
